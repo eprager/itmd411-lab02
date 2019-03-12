7005a3ed546abeb211f3132cf09ce72e858431ac
Emma Prager <46731977+eprager@users.noreply.github.com>
2019-03-12 18:39:54 -0500

    Final Lab 02

---
+++ Lab 02/src/BankRecordsTest.java
@@ -0,0 +1,16 @@
+/*
+ * Emma Prager
+ * 02/28/2019
+ * BankRecordsTest.java
+ * Lab 02
+ */
+
+public class BankRecordsTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BankRecords test = new BankRecords();
+		test.readData();
+	}
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # itmd411-lab2
+
+Emma Prager
+
+ITMD 411 - Lab 2
+
+Illinois Tech
